b7a7f0837bc04dcbfac129be2ef3a9aed9082f0c
Gabriel Gonzalez <Gabriel439@gmail.com>
2016-07-25 09:36:54 -0700

    Add Travis support (via Nix) (#3)
    * Add Travis CI support
    
    * Pin GHC version to 7.10.3

--- release.nix
+++ release.nix
@@ -1,4 +1,13 @@
-{ pkgs ? import <nixpkgs> {}, compiler ? "ghc7102" }:
-{   proto3-wire =
-        pkgs.haskell.packages.${compiler}.callPackage ./default.nix { };
-}
+let config = {
+  packageOverrides = pkgs: {
+    haskellPackages = pkgs.haskell.packages.ghc7103.override {
+      overrides = haskellPackagesNew: haskellPackagesOld: {
+        proto3-wire = haskellPackagesOld.callPackage ./default.nix { };
+      };
+    };
+  };
+};
+in
+
+{ pkgs ? import <nixpkgs> { inherit config; } }:
+pkgs.haskellPackages.proto3-wire
