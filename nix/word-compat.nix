8bbd3e2a90575f1d88bfbdd36ce97ed8504ee1ba
John Carey <jcarey@arista.com>
2022-03-18 16:08:10 -0700

    Use patched word-compat and avoid compiler warnings.

--- nix/word-compat.nix
+++ nix/word-compat.nix
@@ -3,9 +3,9 @@ mkDerivation {
   pname = "word-compat";
   version = "0.0.1";
   src = fetchgit {
-    url = "https://github.com/fumieval/word-compat";
-    sha256 = "13370fp55ms3bmyip83vav33jasggz04sksy61ypd0fm5yirpj7x";
-    rev = "c02432cea08bc8037fe8f1bbfc7a026bbddff75e";
+    url = "https://github.com/j6carey/word-compat";
+    sha256 = "03g8lz806h3m15dg6894snyizav76skkfw9bxxa1b5wlyq6aqag1";
+    rev = "1d3147cb6789b4ad67285dfbe8b2384c8842b6f1";
     fetchSubmodules = true;
   };
   libraryHaskellDepends = [ base ghc-prim ];
