f4b8e73773f2d9822d96b543f60b166d28134cd3
John Carey <jcarey@arista.com>
2022-03-19 12:32:03 -0700

    Hackage now has word-compat-0.0.2; use it.

--- nix/word-compat.nix
+++ nix/word-compat.nix
@@ -1,13 +1,8 @@
-{ mkDerivation, base, fetchgit, ghc-prim, lib }:
+{ mkDerivation, base, ghc-prim, lib }:
 mkDerivation {
   pname = "word-compat";
-  version = "0.0.1";
-  src = fetchgit {
-    url = "https://github.com/j6carey/word-compat";
-    sha256 = "03g8lz806h3m15dg6894snyizav76skkfw9bxxa1b5wlyq6aqag1";
-    rev = "1d3147cb6789b4ad67285dfbe8b2384c8842b6f1";
-    fetchSubmodules = true;
-  };
+  version = "0.0.2";
+  sha256 = "36e5a1a17f20935f55bf274fb52cbb028d301fd48d814f574d3171ccc6bc9f98";
   libraryHaskellDepends = [ base ghc-prim ];
   description = "Compatibility shim for the Int/Word internal change in GHC 9.2";
   license = lib.licenses.bsd3;
