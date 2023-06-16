c185c34ada4aeada27d9582a53db8f127e04ebd8
Raghu Kaippully <raghuk@arista.com>
2023-06-16 11:16:46 +0530

    Bump nixpkgs to 21.11

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,12 +1,9 @@
 args:
 
 let
-  # nixpkgs release 21.05
-  rev = "7e9b0dff974c89e070da1ad85713ff3c20b0ca97";
-  sha256 = "1ckzhh24mgz6jd1xhfgx0i9mijk6xjqxwsshnvq789xsavrmsc36";
-
   nixpkgs = builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-    inherit sha256;
+    # nixos-21.11 as on 2023-06-16
+    url = "https://github.com/NixOS/nixpkgs/archive/eabc38219184cc3e04a974fe31857d8e0eac098d.tar.gz";
+    sha256 = "04ffwp2gzq0hhz7siskw6qh9ys8ragp7285vi1zh8xjksxn1msc5";
   };
 in import nixpkgs ({ config = { }; } // args)
