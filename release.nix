4160adc5bcfd8302f5615f7d627c44c4c8f00803
Raghu Kaippully <raghuk@arista.com>
2023-07-17 09:32:28 +0530

    Support multiple compiler versions

--- release.nix
+++ release.nix
@@ -1,6 +1,9 @@
-let
-  pkgs = import ./nix/pkgs.nix;
+{ compiler ? "ghc8107" }:
 
+let
+  pkgs = import ./nix/pkgs.nix {
+    inherit compiler;
+  };
 in {
   proto3-wire = pkgs.haskell.lib.buildStrictly pkgs.haskellPackages.proto3-wire;
 }
